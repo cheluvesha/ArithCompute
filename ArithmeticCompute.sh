381fdf15bc3277b339298046d5b1b5ad57f75557
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 12:40:36 +0530

    addition with multiplication

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -1,4 +1,16 @@
 #!/bin/bash -x
+#UC1
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+
+#UC2
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $c+$a/$b ))
+
+#UC3
 read -p "Enter a value for a: " a
 read -p "Enter a value for b: " b
 read -p "Enter a value for c: " c
