d55152f93f6bcff45d677ca1c389431452501d0e
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 12:47:00 +0530

    Store result in Dictionary

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -1,4 +1,34 @@
 #!/bin/bash -x
+#UC1
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+
+#UC2
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a+$b*$c ))
+
+#UC3
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a*$b+$c ))
+
+#UC4
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $c+$a/$b ))
+
+#UC5
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a%$b+$c ))
+
+#UC6
 read -p "Enter a value for a: " a
 read -p "Enter a value for b: " b
 read -p "Enter a value for c: " c
