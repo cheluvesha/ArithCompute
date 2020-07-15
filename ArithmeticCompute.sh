7e23b8709f558282095ba9c280aa7f438da82fd8
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 12:49:16 +0530

    Displaying in Descending

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -1,4 +1,60 @@
 #!/bin/bash -x
+#UC1
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+
+#UC2
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a+$b*$c ))
+
+#UC3
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a*$b+$c ))
+
+#UC4
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $c+$a/$b ))
+
+#UC5
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a%$b+$c ))
+
+#UC6
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+declare -A computation
+computation[add]=$(( $a+$b*$c ))
+computation[mul]=$(( $a*$b+$c ))
+computation[div]=$(( $c+$a/$b ))
+computation[mod]=$(( $a%$b+$c ))
+
+#UC7
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+declare -A computation
+computation[1]=$(( $a+$b*$c ))
+computation[2]=$(( $a*$b+$c ))
+computation[3]=$(( $c+$a/$b ))
+computation[4]=$(( $a%$b+$c ))
+index=0;
+for i in ${!computation[@]}
+do
+        array[((index++))]=${computation[$i]}
+done
+echo ${array[@]}
+
+#UC8
 read -p "Enter a value for a: " a
 read -p "Enter a value for b: " b
 read -p "Enter a value for c: " c
