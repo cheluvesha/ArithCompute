6c85f35acecc2078de1d93e523c9da4401fd1451
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 12:44:58 +0530

    addition with division

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -8,4 +8,16 @@ read -p "Enter a value for c: " c
 read -p "Enter a value for a: " a
 read -p "Enter a value for b: " b
 read -p "Enter a value for c: " c
+echo $(( $a+$b*$c ))
+
+#UC3
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+echo $(( $a*$b+$c ))
+
+#UC4
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
 echo $(( $c+$a/$b ))
