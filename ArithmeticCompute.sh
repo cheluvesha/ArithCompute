b720e27c8e2c97dabf6f486968bb0dd9e5fa5e1e
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 12:35:54 +0530

    Update ArithmeticCompute.sh

--- ArithmeticCompute.sh
+++ ArithmeticCompute.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -x
+#UC-1
+read -p "Enter a value for a: " a
+read -p "Enter a value for b: " b
+read -p "Enter a value for c: " c
+
+#UC-2
 read -p "Enter a value for a: " a
 read -p "Enter a value for b: " b
 read -p "Enter a value for c: " c
